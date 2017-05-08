3ad07117ea99ad8c991d3017a10e5d1599bc1c00
Grey Patterson <greypatterson@gmail.com>
2017-05-08 11:06:26 -0700

    Initial Commit

---
+++ ProjectFour/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ProjectFour
+//
+//  Created by Grey Patterson on 2017-05-08.
+//  Copyright © 2017 Grey Patterson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
