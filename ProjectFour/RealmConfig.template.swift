3201f31c94dcaaf5fdc98e81e88255d3f2a151fe
Grey Patterson <greypatterson@gmail.com>
2017-05-12 11:23:22 -0700

    Add config template

---
+++ ProjectFour/RealmConfig.template.swift
@@ -0,0 +1,20 @@
+//
+//  RealmConfig.template.swift
+//  ProjectFour
+//
+//  Created by Grey Patterson on 2017-05-12.
+//  Copyright © 2017 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+/*
+struct login{
+    static let username = ""
+    static let password = ""
+    static let serverURL = URL(string: "")!
+}
+
+struct google{
+    static let APIKey = ""
+}
+ */
